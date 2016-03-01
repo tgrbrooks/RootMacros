061368c73a325ce74a5b275378ee8eca382a7c97
Luke Simons <luke.simons@student.manchester.ac.uk>
2016-03-01 10:27:53 +0000

    Problem with ShowerPlotter1D.cxx correct Y-axis label

--- ShowerPlotter1D.cxx
+++ ShowerPlotter1D.cxx
@@ -13,23 +13,54 @@
 
 // Root Macro Includes
 #include "ReadTree.cc"
-#include "TTreeOneDHist.cxx"
-#include "TTreeVsEnergyHist.cxx"
-#include "TTreeEnergyAndOneD.cxx"
+//#include "TTreeOneDHist.cxx"
+//#include "TTreeVsEnergyHist.cxx"
+//#include "TTreeEnergyAndOneD.cxx"
+
+
+//example of macro illustrating how to superimpose two histograms
+#include "TCanvas.h"
+#include "TStyle.h"
+#include "TH1.h"
+#include "TGaxis.h"
+#include "TRandom.h"
+#include "TFile.h"
+#include "TLegend.h"
+#include "TChain.h"
+
+// Function to plot a stacked histogram of all neutrino energies split by their flavours
+void TTreeOneDHist(TChain &RootTree,string Variable){
+ 
+  // Create new canvas
+  TCanvas *c1 = new TCanvas(Variable.c_str(),"TTreeOneDHist Canvas",900,600);
+  RootTree.Draw(Variable.c_str(),"Evec<500","",10000000000,0); // Looking for E < 500MeV showers
+  c1->cd(); 
+
+  double per = (htemp->GetXaxis()->GetXmax())/(htemp->GetSize())
+  std::stringstream ss;
+  ss << "Events / (" << fixed << setprecision( 1 ) << per << ")";
+
+  // Draw histograms and adjust axis
+  htemp->GetXaxis()->SetTitle(Variable.c_str());  htemp->GetXaxis()->SetTitleOffset(0.9);
+  htemp->GetYaxis()->SetTitle((ss.str()).c_str());       htemp->GetYaxis()->SetTitleOffset(0.9);
+  htemp->Draw();
+
+  c1->Update();  c1->Modified();
+}
 
 // Main Script
 void ShowerPlotter1D(){
 
   const int NumOfVars = 52;
   string CutVariables[NumOfVars] = {
-    "hitNo","hitNoU","hitNoV","hitNoY","TDCstd","TDCstdU","TDCstdV","TDCstdY","TDCiqr","TDCiqrU","TDCiqrV","TDCiqrY",
+    "Evec","hitNo","hitNoU","hitNoV","hitNoY","TDCstd","TDCstdU","TDCstdV","TDCstdY","TDCiqr","TDCiqrU","TDCiqrV","TDCiqrY",
     "ADCamp","ADCampU","ADCampV","ADCampY","WFint","WFintU","WFintV","WFintY","Meanamp","MeanampU","MeanampV","MeanampY",
     "Meanint","MeanintU","MeanintV","MeanintY","LowDen","LowDenU","LowDenV","LowDenY","HiDen","HiDenU","HiDenV","HiDenY",
     "MeanRMS","MeanRMSU","MeanRMSV","MeanRMSY","MeanMult","MeanMultU","MeanMultV","MeanMultY",
     "Wirestd","WirestdU","WirestdV","WirestdY","Wireiqr","WireiqrU","WireiqrV","WireiqrY" };
 
   // Create list to contain shower files to be merged
-  const int NumShowerFiles = 100;  string ShowerFileNames[NumShowerFiles];
+  const int NumShowerFiles = 195;  string ShowerFileNames[NumShowerFiles];
  
   // Define String stream and temporary string to hold the file number
   stringstream ss; string FileNumAsString;
@@ -69,7 +100,8 @@ void ShowerPlotter1D(){
 
   myfile << "\n\n" << "Now plotting 1D Histograms of all cut variables.";
   for(unsigned int i(0); i < 6; i ++){ // < NumOfVars
-    TTreeEnergyAndOneD(ShowerTrees,CutVariables[i]);
+//    TTreeEnergyAndOneD(ShowerTrees,CutVariables[i]);
+    TTreeOneDHist(ShowerTrees,CutVariables[i]);
   }
   myfile << "\n" << "Plotting 1D Histograms completed.\n\n ";
   myfile << "\n" << "Finished writing to file. ShowerPlotter1D completed successfully!\n\n ";
